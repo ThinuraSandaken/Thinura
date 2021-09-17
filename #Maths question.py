234d7362752136daea729c9392ce82f1bf807916
ThinuraSandaken <90761685+ThinuraSandaken@users.noreply.github.com>
2021-09-17 19:14:25 +0530

    This a math question
    Try and enjoy.

---
+++ #Maths question.py
@@ -0,0 +1,111 @@
+#Maths question
+
+x = 20
+y = 45
+z = -23
+
+print("x = 20")
+print("y = 45")
+print("z = -23")
+
+que1 = x * 5 - y
+print ("(1).x * 5 - y ")
+
+print("1) 45")
+print("2) 55")
+print("3) 64")
+print('Please choose a answer.')
+
+que1 = input("Please enter your answer:\n")
+que1 = int(que1)
+
+if que1 == 55:
+	print("Answer is correct")
+elif  que1 == 45:
+	print("Answer is incorrect.This is the answer = 55")
+	print(" 20 * 5 = 100")
+	print("100 - 45 = (55)")
+elif  que1 == 64:
+	print("Answer is incorrect.This is the answer = 55")
+	print(" 20 * 5 = 100")
+	print("100 - 45 = (55)")
+
+que2 = input("Would you like to face the next question ?\n")
+
+if que2 == 'yes':
+	print("ok")
+else:
+	print("Good bye")
+
+if que2 == 'yes':
+	print("This is the second question.")
+	print("x = 20")
+	print("y = 45")
+	print("z = -23")
+	print(" x * y - z + 100 ")
+	print("1). 880")
+	print("2). 977")
+	print("3). 980")
+	
+x = 20
+y = 45
+z = -23
+
+que2 = input("Please choose a answer...\n")
+que2 = int(que2)
+
+if que2 == '977':
+	print("Your answer is correct")
+elif que2 == '980':
+	print("Your answer is incorrect")
+	print("20 * 45 = 900 ")
+	print("900 - 23 = 877")
+	print("877 + 100 = 977")
+elif que2 == '880':
+	print("Your answer is incorrect")
+	print("20 * 45 = 900 ")
+	print("900 - 23 = 877")
+	print("877 + 100 = 977")
+
+que3 = input("Would you like this programe...?\n")
+
+if que3 == 'yes':
+	print("Thanks for your vote ...")
+else:
+	print("It's ok .Thanks.")
+
+x = 20
+y = 45
+z = -23
+
+if que3 ==	'yes':
+	print("This is your third question...")
+	print("x = 20")
+	print("y = 45")
+	print("z = -23")
+	print("1250 / x - z * 2 ")
+	print("1). 79")
+	print("2). 108")
+	print("3). 63")
+
+que3 = input("Please choose a answer...\n")
+que3 = int(que3)
+
+if que3 == '79':
+	print("Your answer is correct.")
+elif que3 == '108':
+	print("Your answer is incorrect")
+	print("1250 / 20 = 62.5")
+	print("62.5 - 23 = 39.5")
+	print("39.5 * 2 = 79")
+elif que3 == '63':
+	print("Your answer is incorrect")
+	print("1250 / 20 = 62.5")
+	print("62.5 - 23 = 39.5")
+	print("39.5 * 2 = 79")
+
+print("Thank for using this software.")
+print("The end.......")
+	
+	
+	
