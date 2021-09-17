3aa90a4f2c455cc005cf99a93c8f8d0637144776
ThinuraSandaken <90761685+ThinuraSandaken@users.noreply.github.com>
2021-09-17 19:12:32 +0530

    This is mark guiding programe
    Learn and write

---
+++ #Marks  guinding.py
@@ -0,0 +1,17 @@
+#mark guiding
+
+mark = input("Please enter your English mark:\n")
+
+mark = int(mark)
+
+if mark >= 75:
+	print("A pass")
+elif mark >= 65:
+	print("B pass")
+elif mark >= 50:
+	print("C pass")
+elif mark >= 35:
+	print("S pass")
+elif mark >= 1:
+	print("Fail")
+exit;	
